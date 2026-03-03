b586fa2cfecc5823d687a3f20138419db326ae11
Sujal Karki <sujalkarki1904@gmail.com>
2026-03-03 20:18:43 +0545

    Routing setup in frontned

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './Components/Navbar'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Homepage from './pages/Homepage'
 const App = () => {
   return (
-    <div>App</div>
+   <>
+   <Navbar />
+   <Routes>
+
+   </Routes>
+   </>
   )
 }
 
